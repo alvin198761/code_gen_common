f4c30aff9a4cf672f5bcef5ad02435fd838a8154
tangzhichao <alvin198761@163.com>
2019-01-08 17:30:36 +0800

    修改实体类

--- src/main/java/org/alvin/code/gen/beans/RestfullResp.java
+++ src/main/java/org/alvin/code/gen/beans/RestfullResp.java
@@ -8,8 +8,6 @@ import lombok.Data;
 @Data
 public class RestfullResp<T> {
 
-	@ApiModelProperty(value = "是否成功", notes = "true = 成功，false=不成功", required = true)
-	private boolean success = true;
 	@ApiModelProperty(value = "失败原因", notes = "失败内容")
 	private String errorMsg;
 	@ApiModelProperty(value = "成功响应结果实体", notes = "错误信息", required = true)
@@ -21,14 +19,12 @@ public class RestfullResp<T> {
 	}
 
 	public RestfullResp(T data) {
-		this.success = true;
 		this.errorMsg = null;
 		this.data = data;
 		this.code = 0;
 	}
 
 	public RestfullResp(Integer code, String errorMsg) {
-		this.success = false;
 		this.errorMsg = errorMsg;
 		this.code = code;
 		this.data = null;
